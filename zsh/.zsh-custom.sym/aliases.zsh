d6a91f4534058ef36ae9d92c339a6904ffa2d680
Azzapop <aaron.hook@siteminder.com>
2021-04-08 09:48:05 +1000

    Add work changes for new laptop

--- zsh/.zsh-custom.sym/aliases.zsh
+++ zsh/.zsh-custom.sym/aliases.zsh
@@ -25,3 +25,22 @@ alias gl='/usr/bin/git log --date-order --graph --pretty=format:'\''%Cred%h%Cres
 alias be="bundle exec"
 alias rails="bundle exec rails"
 alias rake="bundle exec rake"
+
+# Docker aliases
+alias docker-down="docker-compose down"
+alias docker-logs="docker-compose exec app tail -f log/development.log"
+alias docker-exec="docker-compose exec app"
+alias docker-up="docker-compose up"
+alias docker-run="docker-compose run"
+alias docker-build="docker-compose build"
+alias lh-run="docker-down && docker-compose up -d && sleep 1 && docker-logs"
+
+# Connection aliases
+alias connect-app-stg="ssh appukror01.stg.siteminder.com"
+alias connect-app-preprod="ssh appukror11.preprod.siteminder.com"
+alias connect-app-prod="ssh appukror01.uswest2.siteminder.com"
+alias connect-apac-stg="ssh appauror01.stg.siteminder.com"
+alias connect-apac-preprod="ssh appauror11.preprod.siteminder.com"
+alias connect-apac-prod="ssh appauror01.uswest2.siteminder.com"
+
+alias ssh_cleanup="pkill -KILL -f 'ssh:|ssh-agent'; launchctl stop com.openssh.sshd; rm -Rf ~/.ssh/connections/*;"
